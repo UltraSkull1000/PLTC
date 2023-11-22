24d9060fa9a56db09f2ec685e9936093bebd4670
McElroy Ruman <ultraskull1000@gmail.com>
2023-11-22 14:09:21 -0500

    DatabaseConnection

---
+++ deployment/server/database/dbconnection.js
@@ -0,0 +1,24 @@
+require('dotenv').config({ path: require('find-config')('.env') });
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.ATLAS_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("pltcg").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
