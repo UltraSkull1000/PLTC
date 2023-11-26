0307f122223de948b7cfdf4d773beb1c6a4a026f
TerryDavisFan <145712564+TerryDavisFan@users.noreply.github.com>
2023-11-26 14:56:11 -0500

    Update trade.js

--- deployment/server/models/trade.js
+++ deployment/server/models/trade.js
@@ -1,1 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+const User = require('./users');
+const Player = require('./player');
 
+const tradeSchema = new Schema({
+    sender: {
+        type: String,
+        required: true
+    },
+    reciever: {
+        type: String,
+        required: true
+    },
+    offer: {
+        type: [String],
+        required: true
+    },
+    request: {
+        type: [String],
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    }
+});
+
+tradeSchema.plugin(passportLocalMongoose);
+
+const Trade = mongoose.model('Trade', tradeSchema);
+module.exports = Trade;
