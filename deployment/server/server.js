7db5e9216568555ff14b9f2abdd1d0fcdf56fc32
TerryDavisFan <145712564+TerryDavisFan@users.noreply.github.com>
2023-11-26 18:19:38 -0500

    Update server.js

--- deployment/server/server.js
+++ deployment/server/server.js
@@ -73,6 +73,44 @@ app.post('/register', async (req, res) => {
     res.status(200).send("Successfully Posted User!");
 })
 
+app.put("/:sessionKey/quicksell", (req, res) => {
+    var user = User.findOne({ sessionKey: req.params.sessionKey});
+    var card = res.body.card;
+    if(user.collection.indexOf(card) == -1 && user.dupes.indexOf(card) == -1)
+    {
+        res.status(400).json({ error: "User does not have this card" });
+    }
+    else
+    {
+        var money = 0;
+        var rarity = card.rarity;
+        switch(rarity) {
+            case 0: //common
+                money = 100;
+                break;
+            case 1: //rare
+                money = 300;
+                break;
+            case 2: //pro
+                money = 1000;
+                break;
+            case 3: // world class
+                money = 5000;
+                break;
+        }
+        user.money = user.money + money;
+        //user is selling a dupe
+        if(user.dupes.indexOf(card) != -1)
+        {
+            user.dupes.splice(user.dupes.indexOf(card), 1);
+        }
+        else
+        {
+            user.collection.splice(user.collection.indexOf(card), 1);
+        }
+    }
+});
+
 app.post('/validatecards', async (req, res) => {
     const dbc = require("./database/dbconnection");
     await dbc.validateCards();
