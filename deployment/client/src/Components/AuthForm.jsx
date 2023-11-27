77ba4840488690cd5249f80f9a51d530d5a82c1c
samsondavid381 <136836294+samsondavid381@users.noreply.github.com>
2023-11-26 21:33:32 -0500

    remove mt-10

--- deployment/client/src/Components/AuthForm.jsx
+++ deployment/client/src/Components/AuthForm.jsx
@@ -4,7 +4,7 @@ export default function AuthForm() {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
         </div>
